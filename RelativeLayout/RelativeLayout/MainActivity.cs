2cf17a71fcdac1075b5a6342d203fadce0a2cf12
Gabriel <sk9g3.go@gmail.com>
2017-07-22 19:59:01 -0300

    App de exemplo com relativeLayout

---
+++ RelativeLayout/RelativeLayout/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace RelativeLayout
+{
+    [Activity(Label = "RelativeLayout", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        Button btnConverter;
+         EditText txtDolar;
+         EditText txtReal;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView (Resource.Layout.Main);
+
+            btnConverter = FindViewById<Button>(Resource.Id.btn_converter);
+            txtDolar = FindViewById<EditText>(Resource.Id.txt_dolar);
+            txtReal = FindViewById<EditText>(Resource.Id.txt_real);
+
+            btnConverter.Click += BtnConverter_Click;
+        }
+
+        private void BtnConverter_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                double dolar = double.Parse(txtDolar.Text);
+                double real = dolar * 3.25;
+                txtReal.Text = real.ToString();
+            }catch(Exception ex)
+            {
+                Toast.MakeText(this, ex.Message, ToastLength.Long).Show();
+            }
+        }
+    }
+}
+
